5cf44bd33fdf6998fbc3747244091c117dde9830
482F <482F.git.mail@482f.net>
2021-10-07 00:00:00 +0900

    任意の関数が true になるまで待つメソッドを追加

--- src/utls.js
+++ src/utls.js
@@ -15,3 +15,14 @@ export function listenMessage(label, handler) {
 export function sendMessage(label, content) {
   chrome.runtime.sendMessage({ label, content })
 }
+
+export async function wait(func, intervalMs = 100, timeoutMs = 0) {
+  const startMs = new Date().getTime()
+  while (!func()) {
+    await new Promise((resolve) => setTimeout(resolve, intervalMs))
+    if (timeoutMs && timeoutMs < new Date().getTime() - startMs) {
+      return false
+    }
+  }
+  return true
+}
