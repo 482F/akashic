3f182b1a11420d86979c9312b87cc67c473a5abf
482F <482F.git.mail@482f.net>
2021-10-08 00:00:00 +0900

    history の概要を表示するコンポーネントを作成

---
+++ src/options/historyOverview.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-list-item :href="history.url" v-if="ready" @click.prevent="">
+    <v-list-item-icon>
+      <v-img :src="history.faviconSrc" class="inline-block" />
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title v-text="history.name" />
+      <v-list-item-subtitle>
+        <v-chip v-for="(tag, i) in Object.keys(history.tags)" :key="i" small>
+          {{ tag }}
+        </v-chip>
+      </v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-action>
+      <v-list-item-action-text v-text="accesseDate" />
+      <v-icon @click="switchStar">
+        {{ "mdi-star" + (history.star ? "-outline" : "") }}
+      </v-icon>
+    </v-list-item-action>
+  </v-list-item>
+</template>
+
+<script>
+import { dateToText, faviconIntArrToImgSrc } from "@utls/miscs.js"
+import { getValueById, updateValueById } from "@utls/storages.js"
+
+export default {
+  name: "history-overview",
+  mounted() {
+    this.init()
+  },
+  data() {
+    return {
+      history: undefined,
+      ready: false,
+    }
+  },
+  props: {
+    rawHistory: {
+      type: Object,
+      default: undefined,
+    },
+  },
+  computed: {
+    accesseDate() {
+      return dateToText(new Date(this.rawHistory.date))
+    },
+  },
+  methods: {
+    async init() {
+      if (!this.rawHistory) {
+        return
+      }
+      this.history = await getValueById("page", this.rawHistory.id)
+      const domain = await getValueById("domain", this.history.domain)
+      this.history.faviconSrc = faviconIntArrToImgSrc(domain.favicon)
+      this.ready = true
+    },
+    async switchStar() {
+      this.history.star = !this.history.star
+      await updateValueById("page", this.rawHistory.id, {
+        star: this.history.star,
+      })
+    },
+    middleClicked() {
+      window.open(this.history.url)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/options/historyOverviews.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-list two-line class="overviews">
+    <v-list-item-group v-model="activeItem">
+      <history-overview
+        v-for="(rawHistory, i) in rawHistories"
+        :key="i"
+        :rawHistory="rawHistory"
+      />
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+import HistoryOverview from "./historyOverview.vue"
+
+export default {
+  name: "history-overviews",
+  components: {
+    HistoryOverview,
+  },
+  data() {
+    return {
+      activeItem: undefined,
+    }
+  },
+  props: {
+    rawHistories: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  watch: {
+    activeItem(value) {
+      this.$emit("select", this.rawHistories[value])
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.overviews {
+  height: 80vh;
+  @apply overflow-auto;
+  scrollbar-width: thin;
+  scrollbar-color: #6969dd #e0e0e0;
+}
+
+.overviews::-webkit-scrollbar {
+  width: 10px;
+}
+
+.overviews::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+  border-radius: 100px;
+}
+
+.overviews::-webkit-scrollbar-thumb {
+  background-color: #a8a8a8;
+  border-radius: 100px;
+}
+</style>
