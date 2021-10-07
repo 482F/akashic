f5ff9f60293ba0afeba4d7088f6c43b822e5d0c3
482F <482F.git.mail@482f.net>
2021-10-08 00:00:00 +0900

    contents-script から送られてきた情報を適切に storage に格納するように

--- src/background.js
+++ src/background.js
@@ -1,8 +1,69 @@
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("Hello from the background")
-  console.log(request, sender, sendResponse)
+import { sleep } from "@utls/asyncs.js"
+import {
+  setValueByKey,
+  getValueByKey,
+  setValueById,
+  pushToList,
+} from "@utls/storages.js"
+import { getFaviconIntArr } from "@utls/miscs.js"
+import { listenMessage } from "@utls/messages.js"
 
-  browser.tabs.executeScript({
-    file: "content-script.js",
+const waitingLine = []
+
+async function main() {
+  chrome.commands.onCommand.addListener((command) => {
+    if (command === "openConfig") {
+      chrome.tabs.create({
+        url: "chrome-extension://" + chrome.runtime.id + "/options.html",
+      })
+    } else if (command === "reload") {
+      chrome.runtime.reload()
+    }
   })
-})
+
+  listenMessage("accessed", addWaitingLine)
+  for (;;) {
+    await sleep(1000)
+    if (waitingLine.length) {
+      await waitingLine.shift()()
+    }
+  }
+}
+
+function addWaitingLine(obj) {
+  waitingLine.push(async function () {
+    await setHistory(obj)
+  })
+}
+
+async function setHistory(obj) {
+  let history = await getValueByKey("page", obj.url)
+  const domain = new URL(obj.url).hostname
+  if (!history) {
+    history = {
+      url: obj.url,
+      domain: undefined,
+      name: obj.name,
+      star: false,
+      accessDates: [],
+      tags: {},
+    }
+  }
+  history.accessDates.push(new Date().getTime())
+  const pageId = await setValueByKey("page", obj.url, history)
+  await pushToList("history", pageId)
+  let domainObj = await getValueByKey("domain", domain)
+  if (!domainObj) {
+    domainObj = {
+      domain,
+      favicon: await getFaviconIntArr(obj.url),
+      pages: {},
+    }
+  }
+  domainObj.pages[pageId] = null
+  const domainId = await setValueByKey("domain", domain, domainObj)
+  history.domain = domainId
+  await setValueById("page", pageId, history)
+}
+
+main()
