759e4f2d198c82754842f8684756bfd792be59e6
482F <482F.git.mail@482f.net>
2021-10-08 00:00:00 +0900

    history を表示するページを作成

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,19 +1,73 @@
 <template>
-  <hello-world />
+  <v-app>
+    <v-container class="root">
+      <v-row>
+        <v-text-field filled class="search" />
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <history-overviews
+            :rawHistories="rawHistories.value"
+            @select="(value) => (activeHistory = value)"
+          />
+        </v-col>
+        <v-col cols="6">
+          <history-detail :rawHistory="activeHistory" />
+        </v-col>
+      </v-row>
+      <v-row class="pagination">
+        <v-pagination :length="6" />
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue"
+import { assignAsyncGenerator } from "@utls/asyncs.js"
+import { getListContentsReverse } from "@utls/storages.js"
+import HistoryOverviews from "./historyOverviews.vue"
+import HistoryDetail from "./historyDetail.vue"
 
 export default {
   name: "App",
-  components: { HelloWorld },
+  components: {
+    HistoryOverviews,
+    HistoryDetail,
+  },
+  mounted() {
+    this.init()
+  },
+  data() {
+    return {
+      rawHistories: {
+        value: [],
+      },
+      activeHistory: undefined,
+    }
+  },
+  methods: {
+    async init() {
+      assignAsyncGenerator(
+        getListContentsReverse("history"),
+        this.rawHistories,
+        "value"
+      )
+    },
+  },
 }
 </script>
 
-<style src="@/tailwind.css">
-html {
-  width: 400px;
-  height: 400px;
+<style lang="scss" scoped>
+.search {
+  height: 5vh;
+}
+.root {
+  @apply my-8;
+  @apply border;
+}
+.pagination {
+  height: 5vh;
+  @apply border;
+  @apply justify-center;
 }
 </style>
