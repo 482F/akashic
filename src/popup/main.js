162d7d765c35be8926c9b6b539f4c4f3728815e7
482F <482F.git.mail@482f.net>
2021-10-08 00:00:00 +0900

    popup で detail を表示するように

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue"
 import App from "./App.vue"
+import Vuetify from "@/plugins/vuetify"
+import "@/tailwind.css"
+
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
+  vuetify: Vuetify,
   render: (h) => h(App),
 })
