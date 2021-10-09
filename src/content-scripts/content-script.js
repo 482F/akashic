3ca840b717326e921ae42762753588bd65b3b52a
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2021-10-09 21:22:21 +0900

    document.title が 3 秒間変わらなかったら background に通知するように
    Twitter が開いた直後は "" で、その後 "Twitter" になった後に
    正しいタイトルになるため

--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,9 +1,18 @@
+import { wait, sleep } from "@utls/asyncs.js"
 import { sendMessage } from "@utls/messages.js"
 
 async function main() {
+  let title = document.title
+  await sleep(3000)
+  await wait(() => {
+    if (title === document.title) {
+      return true
+    }
+    title = document.title
+  }, 3000)
   sendMessage("accessed", {
     url: location.href,
-    name: document.title,
+    name: title,
   })
 }
 
