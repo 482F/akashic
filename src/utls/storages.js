a6215e006be0acdf8b80042a46c854af9e977d96
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2021-10-10 07:09:11 +0900

    chrome.storage でリストに追加/から取得するような関数を追加
    実体はリストの項目数が maxListContentNumber 以上になったら次のキーへ
    移るが、使用する側はそれを意識しなくても良いようになっている

--- src/utls/storages.js
+++ src/utls/storages.js
@@ -26,3 +26,45 @@ export async function getStorage(...keys) {
   await wait(() => value !== null)
   return value
 }
+
+const maxListContentNumber = 1000
+const varKey = "var"
+const lengthKey = "length"
+
+export async function pushToList(...keysAndValue) {
+  const keys = keysAndValue?.slice(0, -1)
+  const value = keysAndValue?.slice(-1)?.[0]
+  if (!keys?.length || !value) {
+    return
+  }
+  let length = await getStorage(varKey, lengthKey, ...keys)
+  if (!length) {
+    length = 1
+    await setStorage(varKey, lengthKey, ...keys, length)
+    await setStorage(...keys, length - 1, [])
+  }
+  const list = await getStorage(...keys, length - 1)
+  list.push(value)
+  await setStorage(...keys, length - 1, list)
+  if (maxListContentNumber <= list.length) {
+    await setStorage(varKey, lengthKey, ...keys, length + 1)
+    await setStorage(...keys, length, [])
+  }
+}
+
+export async function* getListContents(...keys) {
+  if (!keys?.length) {
+    return
+  }
+  const length = await getStorage(varKey, lengthKey, ...keys)
+  if (!length) {
+    return
+  }
+  for (let index = 0; index < length; index++) {
+    const contents = await getStorage(...keys, index)
+    for (const content of contents) {
+      yield content
+      await sleep(100)
+    }
+  }
+}
