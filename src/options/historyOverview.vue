7edf695801c6d9f8688243d298099e34eb3b45c7
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2021-10-10 21:46:52 +0900

    star を表示/設定するコンポーネントを追加

--- src/options/historyOverview.vue
+++ src/options/historyOverview.vue
@@ -17,22 +17,22 @@
     </v-list-item-content>
     <v-list-item-action>
       <v-list-item-action-text v-text="accesseDate" />
-      <v-icon @click="switchStar">
-        {{ "mdi-star" + (history.star ? "" : "-outline") }}
-      </v-icon>
+      <star :history="history" />
     </v-list-item-action>
   </v-list-item>
 </template>
 
 <script>
 import { dateToText, faviconIntArrToImgSrc } from "@utls/miscs.js"
-import { getValueById, updateValueById } from "@utls/storages.js"
+import { getValueById } from "@utls/storages.js"
 import Tag from "./tag.vue"
+import Star from "./star.vue"
 
 export default {
   name: "history-overview",
   components: {
     Tag,
+    Star,
   },
   mounted() {
     this.init()
@@ -63,12 +63,6 @@ export default {
       const domain = await getValueById("domain", this.history.domain)
       this.history.faviconSrc = faviconIntArrToImgSrc(domain.favicon)
       this.ready = true
-    },
-    async switchStar() {
-      this.history.star = !this.history.star
-      await updateValueById("page", this.rawHistory.id, {
-        star: this.history.star,
-      })
     },
     middleClicked() {
       window.open(this.history.url)
---
+++ src/options/star.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-icon @click="switchStar">
+    {{ "mdi-star" + (history.star ? "" : "-outline") }}
+  </v-icon>
+</template>
+
+<script>
+import { updateValueByKey } from "@utls/storages.js"
+
+export default {
+  name: "star",
+  props: {
+    history: {
+      type: Object,
+      require: true,
+    },
+  },
+  methods: {
+    async switchStar() {
+      this.history.star = !this.history.star
+      await updateValueByKey("page", this.history.url, {
+        star: this.history.star,
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
