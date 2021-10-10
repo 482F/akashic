eaf12545264aea6071799e60127bd481eb974e68
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2021-10-10 15:36:51 +0900

    miscs に数字の桁埋めをする関数と日付を整形する関数を追加

--- src/utls/miscs.js
+++ src/utls/miscs.js
@@ -23,3 +23,20 @@ export function faviconIntArrToImgSrc(intArr) {
   const data = new Uint8Array(intArr)
   return URL.createObjectURL(new Blob([data], { type: "image/png" }))
 }
+
+export function fillNum(str, digits) {
+  if (digits <= str.length) {
+    return str
+  }
+  return (Array(digits).fill("0") + str).slice(-digits)
+}
+
+export function dateToText(date) {
+  const y = date.getFullYear()
+  const mo = fillNum(date.getMonth(), 2)
+  const d = fillNum(date.getDate(), 2)
+  const h = fillNum(date.getHours(), 2)
+  const mi = fillNum(date.getMinutes(), 2)
+  const s = fillNum(date.getSeconds(), 2)
+  return `${y}/${mo}/${d} ${h}:${mi}:${s}`
+}
